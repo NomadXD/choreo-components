105306fb04ca96b9972e5bf37180359e33f62c0a
Lahiru De Silva <lahirude@wso2.com>
2024-06-28 18:41:30 +0530

    http-retry-nodejs: update port and host
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

---
+++ http-retry-nodejs/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3000;
+
+// Create a write stream (in append mode) for logging
+const logStream = fs.createWriteStream("error.log", { flags: "a" });
+
+// Setup morgan to log requests to both the console and the log file
+app.use(morgan("combined", { stream: logStream }));
+app.use(morgan("dev"));
+
+app.use("/500", (req, res) => {
+  // Log the request manually (optional, as morgan already logs it)
+  console.log(`Request received: ${req.method} ${req.path} ${req.ip}`);
+
+  // Return a 500 status code
+  res.status(500).send("Internal Server Error");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
